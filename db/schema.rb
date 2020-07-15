0b73e457aa3626d786bc28450d347c1a2de3e1c4
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-07-15 18:09:41 -0300

    Add active storage postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_231646) do
+ActiveRecord::Schema.define(version: 2020_07_15_210731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
